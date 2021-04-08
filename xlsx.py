c211e64161e18701ed7850edd571cea695aaf6ac
cksrb0114 <80078788+cksrb0114@users.noreply.github.com>
2021-04-08 23:27:02 +0900

    Update xlsx.py

--- xlsx.py
+++ xlsx.py
@@ -9,3 +9,18 @@ from openpyxl import load_workbook
 from openpyxl import workbook
 import glob
 import os
+
+Myfiles = [i for i in glob('*.xlsx')]
+
+total_student = []
+
+for item in Myfiles:
+    my_workbook = load_workbook(item, data_only = True)
+    my_worksheet = my_workbook['Sheet1']
+    my_list = []
+    my_list.append(my_worksheet['A2'].value)
+    my_list.append(my_worksheet['B2'].value)
+    my_list.append(my_worksheet['C2'].value)
+    my_list.append(my_worksheet['D2'].value)
+    total_student.append(my_list)
+    print(my_list)
